866c2781dbcafe994f5cfa18ffcf45b665a4f38e
Calton Manhique <calton.manhique@dci-student.org>
2023-11-27 14:14:36 +0100

    Initial commit, local repo.

---
+++ src/main/java/com/fitmegut/warehousefinalproject/DciWarehouseFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fitmegut.warehousefinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DciWarehouseFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DciWarehouseFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitmegut/warehousefinalproject/DciWarehouseFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitmegut.warehousefinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DciWarehouseFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
